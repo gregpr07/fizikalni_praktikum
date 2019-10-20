fcabfaaad56bb2e85413b28e87c48b845ff35eea
gregpr07 <gregor.ninjaboy@gmail.com>
2019-10-20 14:01:13 +0200

    graf

--- vaja20/grafi.py
+++ vaja20/grafi.py
@@ -11,64 +11,100 @@ lege_dodajanje = [12.17, 12.22, 12.26, 12.30,
 lege_odstranjanje = [12.45, 12.50, 12.535, 12.57,
                      12.60, 12.64, 12.67, 12.70, 12.73, 12.74]  # cm
 
-# pi*(2r/2)^2
 # 2r podan v mikrom
-presek = ((54/2/(10**6))**2)*math.pi  # m
+radij = 54
 
 dolzina = 1.995  # m
 
 
-def calc_delta(lege):
-    new_list = []
-    for i in range(len(lege)):
-        if i == 0:
-            new_list.append(0)
-        else:
-            new_list.append(lege[i]-lege[0])
-    print(new_list)
-    return(new_list)  # cm
+def draw_graph(mase, lege_dodajanje, radij, dolzina, lege_odstranjanje=[]):
+
+    # pi*(2r/2)^2
+    presek = ((radij/2/(10**6))**2)*math.pi  # m
+
+    def calc_delta(lege, reverse=False):
+        new_list = []
+        for i in range(len(lege)):
+            if reverse:
+                new_list.append(lege[i]-lege[-1])
+            else:
+                new_list.append(lege[i]-lege[0])
+        return(new_list)  # cm
+
+    # N / cm^2 --> /100 je za g v N, /10000 je za m^2 v cm^2
+    x = [(x-mase[0])/100/presek/10000 for x in mase]
+
+    # v cm
+    delte_y = calc_delta(lege_dodajanje)
+
+    # /100 cm -> m
+    y = [(y/100/dolzina) for y in delte_y]  # brez enote
+
+    y_reverse = [y[-1]+(rev/100/dolzina)
+                 for rev in calc_delta(lege_odstranjanje, reverse=True)]
+
+    print(y_reverse)
+    # error
+
+    def calc_error_y(delte, lege_y):
+        abs_y = []
+        for i, lega in zip(delte, lege_y):
+            if i == 0:
+                abs_y.append(0)
+            else:
+                print(0.005/i)
+                abs_y.append((0.01/i)*lega)
+        return abs_y
+
+    def calc_error_x(mass, radius, x_axis):
+        return [(20/x+2*1/radius)*abscisa for x, abscisa in zip(mass, x_axis)]
+
+    napake_y = calc_error_y(delte_y, y)
+    napake_x = calc_error_x(mase, radij, x)
+
+    # find best fit
+
+    def fit_func(x, a):
+        return a*x
+
+    params = curve_fit(fit_func, x[:(len(x)-2)], y[:(len(y)-2)])
+    a = params[0]
+
+    x_fit = np.linspace(x[0], x[-1], 100)
+
+    y_fit = fit_func(x_fit, a)
+
+    # /fit
+
+    print("Calculated E is:", '%.3g' % int(a**-1), 'N/cm^2')
+
+    plt.plot(x, y, 'o', label='Dodajanje uteži')
+    plt.plot(x_fit, y_fit, label=r'$1/E$')
+    plt.errorbar(x, y, xerr=napake_x, yerr=napake_y, fmt='none')
+
+    if lege_odstranjanje:
+        plt.plot(x, y_reverse, '.', label='Odstranjanje uteži')
+        plt.errorbar(x, y_reverse, xerr=napake_x, yerr=napake_y, fmt='none')
+
+        def fit_reverse(x, a, b):
+            return a*x+b
+
+        naklon_reverse = curve_fit(fit_reverse, x, y_reverse)[0][0]
+        print(naklon_reverse)
+        print("Calculated E for reverse is:", '%.3g' %
+              int(naklon_reverse**-1), 'N/cm^2')
+
+    plt.title('Raztezek bakrene zice v odvisnosti od sile')
+    plt.xlabel(r'$(F −F_o)/S$ $[N/cm^2]$')
+    plt.ylabel(r'$\Delta l \: / \: l$')
+
+    plt.grid(True)
+
+    plt.legend()
+    plt.savefig('vaja20/raztezek_baker', dpi=300)
+    print('saved')
+    plt.show()
 
 
-# N / cm^2 --> /100 je za g v N, /10000 je za m^2 v cm^2
-x = [(x-mase[0])/100/presek/10000 for x in mase]
-
-delte_y = calc_delta(lege_dodajanje)
-
-y = [(y/100/dolzina) for y in delte_y]  # brez enote
-
-# error
-
-
-# find best fit
-
-
-def fit_func(x, a):
-    return a*x
-
-
-params = curve_fit(fit_func, x[:(len(x)-2)], y[:(len(y)-2)])
-[a] = params[0]
-
-x_fit = np.linspace(x[0], x[len(x)-1], 100)
-
-y_fit = fit_func(x_fit, a)
-
-
-# /fit
-
-print("Calculated E is:", '%.3g' % int(a**-1), 'N/cm^2')
-
-
-plt.plot(x, y, 'o', label='Meritve')
-plt.plot(x_fit, y_fit, label=r'$1/E$')
-plt.errorbar(x, y)
-
-plt.title('Raztezek bakrene zice v odvisnosti od sile')
-plt.xlabel(r'$(F −F_o)/S$ $[N/cm^2]$')
-plt.ylabel(r'$\Delta l \: / \: l$')
-
-plt.grid(True)
-
-plt.legend()
-plt.savefig('raztezek_baker', dpi=300)
-plt.show()
+draw_graph(mase, lege_dodajanje, radij, dolzina,
+           lege_odstranjanje=lege_odstranjanje)
